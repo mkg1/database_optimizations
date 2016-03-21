8cbe15f1010ac76e73853e14c07164e50723cb53
mkg1 <gehring.mg@gmail.com>
2016-03-21 17:26:58 -0400

    Optimize controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,21 +4,24 @@ class ReportsController < ApplicationController
   def all_data
     @start_time = Time.now
 
-    @sequences = []
-    @genes = []
-    @hits = []
+    # @sequences = []
+    # @genes = []
+    # @hits = []
     @assembly = Assembly.find_by_name(params[:name])
-    @assembly.sequences.each do |s|
-      @sequences << s
-      s.genes.each do |g|
-        @genes << g
-        g.hits.each do |h|
-          @hits << h
-        end
-      end
-    end
-    @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
+    # @assembly.sequences.each do |s|
+    #   @sequences << s
+    #   s.genes.each do |g|
+    #     @genes << g
+    #     g.hits.each do |h|
+    #       @hits << h
+    #     end
+    #   end
+    # end
+    @hits = @assembly.hits
+    # @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
+    # @hits.order(percent_similarity: :desc)
 
+    @hits.where(subject_id: Gene.where(sequence_id: Sequence.where(assembly_id: @assembly.id))).order(percent_similarity: :desc)
     @memory_used = memory_in_mb
   end
 
